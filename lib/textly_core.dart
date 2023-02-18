fdc044a871a7114148bb27218b750694fbee2659
serezhia <dev@serezhia.ru>
2023-02-18 17:13:23 +0300

    add to barrel file

--- lib/textly_core.dart
+++ lib/textly_core.dart
@@ -1,5 +1,6 @@
 /// A Very Good Project created by Very Good CLI.
 library textly_core;
 
+export 'src/models/error_response/error_response.dart' show ErrorResponse;
 export 'src/models/post/post_model.dart' show Post;
 export 'src/models/profile/profile_model.dart' show Profile;
