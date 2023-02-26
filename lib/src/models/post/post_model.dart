718baaa46cec2046ee6f1070b5c29bd3361d1926
serezhia <dev@serezhia.ru>
2023-02-26 23:56:30 +0300

    add fromRedis Post Model

--- lib/src/models/post/post_model.dart
+++ lib/src/models/post/post_model.dart
@@ -29,9 +29,28 @@ class Post with _$Post {
 
   ///
   factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
-////
+  ////
   factory Post.fromPostgres(Map<String, dynamic> row) {
     row['created_at'] = row['created_at'].toString();
+    return _$PostFromJson(row);
+  }
+
+  ///
+  factory Post.fromRedis(Map<String, dynamic> row) {
+    row['created_at'] = row['created_at'].toString();
+
+    row['user_id'] = int.tryParse(row['user_id'] as String? ?? '');
+    row['like_count'] = int.tryParse(row['like_count'] as String? ?? '');
+    row['comments_count'] =
+        int.tryParse(row['comments_count'] as String? ?? '');
+    row['parent_post_id'] =
+        int.tryParse(row['parent_post_id'] as String? ?? '');
+
+    row['is_viewed'] = (row['is_viewed'] as String? ?? 'false') == 'true';
+    row['is_edit'] = (row['is_edit'] as String? ?? 'false') == 'true';
+    row['is_delete'] = row['is_delete'] as String? ?? 'false' == 'true';
+    row['is_liked'] = row['is_liked'] as String? ?? 'false' == 'true';
+    row['is_child'] = row['is_child'] as String? ?? 'false' == 'true';
     return _$PostFromJson(row);
   }
 }
