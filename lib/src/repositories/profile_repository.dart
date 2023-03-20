754dd4d75d47f222ebaa782e09d59cb0ce8f42ee
serezhia <dev@serezhia.ru>
2023-03-21 00:41:52 +0300

    edit profile repository

--- lib/src/repositories/profile_repository.dart
+++ lib/src/repositories/profile_repository.dart
@@ -5,16 +5,16 @@ import 'package:textly_core/src/models/profile/profile_model.dart';
 abstract class ProfileRepository {
   Future<Profile> createProfile({required Profile profile});
   Future<Profile> updateProfile({required Profile profile});
-  Future<Profile> deleteProfile({required int userId});
-  Future<Profile> readProfileForPostByUserId({required int userId});
-  Future<Profile> readProfileByUserId({required int userId});
-  Future<Profile> readProfileByUsername({required String username});
+  Future<void> deleteProfile({required int userId});
+  Future<Profile?> readProfileForPostByUserId({required int userId});
+  Future<Profile?> readProfileByUserId({required int userId});
+  Future<Profile?> readProfileByUsername({required String username});
   Future<List<Profile>> readFollowers({
     required String userId,
     required int offset,
     required int limit,
   });
-  Future<Profile> readFollowing({
+  Future<List<Profile>> readFollowing({
     required String userId,
     required int offset,
     required int limit,
