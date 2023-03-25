85ff4d2dc75d72d49bd282fcc6d0c51c3c6c7e6c
serezhia <dev@serezhia.ru>
2023-03-25 20:54:17 +0300

    refactoring

--- lib/textly_core.dart
+++ lib/textly_core.dart
@@ -2,9 +2,9 @@
 library textly_core;
 
 export 'src/models/oauth_token/token_model.dart' show OAuth2Token;
+export 'src/models/post/post.dart' show Post;
 export 'src/models/profile/profile_model.dart' show Profile;
 export 'src/models/user/user.dart' show User;
-export 'src/models/post/post.dart' show Post;
+export 'src/repositories/post_repository.dart' show PostRepository;
 export 'src/repositories/profile_repository.dart' show ProfileRepository;
 export 'src/repositories/user_repository.dart' show UserRepository;
-export 'src/repositories/post_repository.dart' show PostRepository;
