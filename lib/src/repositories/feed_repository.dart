93d02c3579744c00b877a5e4813efe4064cf6760
serezhia <dev@serezhia.ru>
2023-03-26 11:40:45 +0300

    add feed repo

---
+++ lib/src/repositories/feed_repository.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:textly_core/src/models/post/post.dart';
+
+abstract class FeedRepository {
+  Future<PostsChunk> readPersonalFeed({
+    required int userId,
+    required int offset,
+    required int limit,
+  });
+
+  Future<PostsChunk> readHotFeed({
+    required int offset,
+    required int limit,
+  });
+
+  Future<PostsChunk> readUserPosts({
+    required int userId,
+    required int offset,
+    required int limit,
+  });
+
+  Future<PostsChunk> readUserComments({
+    required int userId,
+    required int offset,
+    required int limit,
+  });
+
+  Future<PostsChunk> readUserLikedPosts({
+    required int userId,
+    required int offset,
+    required int limit,
+  });
+}
