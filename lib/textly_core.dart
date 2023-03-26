c4a0f5a5d31c1569ec579116ab672eda1af34ede
serezhia <dev@serezhia.ru>
2023-03-26 11:50:16 +0300

    add feed repo in dart

--- lib/textly_core.dart
+++ lib/textly_core.dart
@@ -2,9 +2,11 @@
 library textly_core;
 
 export 'src/models/oauth_token/token_model.dart' show OAuth2Token;
-export 'src/models/post/post.dart' show Post;
+export 'src/models/post/post.dart' show Post, PostsChunk;
 export 'src/models/profile/profile_model.dart' show Profile;
 export 'src/models/user/user.dart' show User;
+
+export 'src/repositories/feed_repository.dart' show FeedRepository;
 export 'src/repositories/post_repository.dart' show PostRepository;
 export 'src/repositories/profile_repository.dart' show ProfileRepository;
 export 'src/repositories/user_repository.dart' show UserRepository;
