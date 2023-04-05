552b621d09f75977722b1aa215ac8a19fb3d4efb
serezhia <dev@serezhia.ru>
2023-04-05 19:51:42 +0300

    test fix freezed

--- lib/textly_core.dart
+++ lib/textly_core.dart
@@ -3,7 +3,7 @@ library textly_core;
 
 export 'src/models/oauth_token/token_model.dart' show OAuth2Token;
 export 'src/models/post/post.dart' show Post, PostsChunk;
-export 'src/models/profile/profile_model.dart' show Profile;
+export 'src/models/profile/profile_model.dart' show $ProfileCopyWith, Profile;
 export 'src/models/user/user.dart' show User;
 
 export 'src/repositories/feed_repository.dart' show FeedRepository;
